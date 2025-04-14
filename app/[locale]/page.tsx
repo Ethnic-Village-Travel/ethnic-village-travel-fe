b329529ab82388be96d1049b8a5ca9c98590c897
Gressi-177 <vietdoan177@gmail.com>
2025-04-15 00:49:29 +0700

    feat: integrate next-intl for internationalization support
    - Updated next.config.ts to include next-intl plugin for localization.
    - Added new dependencies: next-intl, @formatjs/intl-localematcher, and negotiator.
    - Updated package.json and yarn.lock to reflect new dependencies and their versions.

---
+++ app/[locale]/page.tsx
@@ -0,0 +1,15 @@
+import { Link } from '@/i18n/navigation';
+import { useTranslations } from 'next-intl';
+
+import LanguageSwitcher from '@/components/language-switcher';
+
+export default function Home() {
+  const t = useTranslations('HomePage');
+  return (
+    <div>
+      <h1>{t('title')}</h1>
+      <Link href="/about">{t('about')}</Link>
+      <LanguageSwitcher />
+    </div>
+  );
+}
