f1d14e39ab082ab12e550440a2e5ff39cef055a1
Gressi-177 <vietdoan177@gmail.com>
2025-05-22 11:41:14 +0700

    feat: enhance Badge component with autoVariant feature and additional color options; update string utility functions with documentation

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,3 +1,22 @@
-export function hasWhiteSpace(s: string) {
-  return /\s/g.test(s);
+/**
+ * Function to check if string has white space
+ * @param str - String to check
+ * @returns Boolean
+ */
+export function hasWhiteSpace(str: string) {
+  return /\s/g.test(str);
+}
+
+/**
+ * Function to hash string to number
+ * @param str - String to hash
+ * @returns Number
+ */
+export function hashStringToNumber(str: string): number {
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    hash = (hash << 5) - hash + str.charCodeAt(i);
+    hash |= 0;
+  }
+  return Math.abs(hash);
 }
