ae79f845eab0fb8339fd81ab71c21343e434be3c
Gressi-177 <vietdoan177@gmail.com>
2025-05-25 14:11:33 +0700

    feat: add page 404

--- src/app/[locale]/(marketing)/tour/[slug]/page.tsx
+++ src/app/[locale]/(marketing)/tour/[slug]/page.tsx
@@ -1,12 +1,20 @@
+import { notFound } from 'next/navigation';
+import { MOCK_TOURS } from '@/data/tours';
+
 interface TourDetailPageProps {
   params: { slug: string };
 }
 
 export default function TourDetail({ params }: TourDetailPageProps) {
   const { slug } = params;
+  const tourData = MOCK_TOURS.find(tour => tour.slug === slug);
+  if (!tourData) {
+    console.log('Tour not found');
+    return notFound();
+  }
   return (
     <div className="flex flex-col items-center justify-center">
-      <h1 className="text-2xl font-bold">Tour Page</h1>
+      <h1 className="text-2xl font-bold">{tourData.title}</h1>
       <p className="mt-4 text-lg">Welcome to the Tour Page!</p>
       <p className="mt-4 text-lg">{slug}</p>
     </div>
