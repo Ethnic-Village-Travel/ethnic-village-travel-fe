06e555054bfeee47f6b67a64769b9c437696fb23
dirtylesc <phanxuansy219@gmail.com>
2025-05-29 19:02:02 +0700

    Handle navigate order page

--- src/components/features/tour/tour-detail/floating-booking-panel/index.tsx
+++ src/components/features/tour/tour-detail/floating-booking-panel/index.tsx
@@ -1,12 +1,21 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
+import { RouteConstant } from '@/constants/route';
+
 import { BookingCalculator } from './booking-calculator';
 import GuildProfileCard from './guild-profile-card';
 
 const FloatingBookingPanel = () => {
+  const router = useRouter();
+
+  const handleBook = () => {
+    router.push(RouteConstant.order);
+  };
+
   return (
     <div className="flex gap-5 xl:w-[360px] xl:flex-shrink-0 xl:flex-col">
-      <BookingCalculator onBook={() => {}} />
+      <BookingCalculator onBook={handleBook} />
       <GuildProfileCard />
     </div>
   );
