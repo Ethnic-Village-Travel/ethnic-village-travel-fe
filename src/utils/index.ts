bdb2c79ccb92126d690a3c255185b49ba15605e6
Gressi-17 <doantv.dev@gmail.com>
2025-11-29 17:30:31 +0700

    chore: Remove demo form component and associated API hook
    - Delete form-demo.tsx component from shared components
    - Remove useDemo.ts hook containing demo API queries
    - Clean up unused utility exports from utils/index.ts
    - Remove deprecated demo functionality to reduce codebase clutter

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,5 @@
 export * from './classnames';
-export * from './key';
 export * from './number';
 export * from './statistics';
 export * from './string';
-export * from './timing';
 export * from './url';
